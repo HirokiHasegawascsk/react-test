e1425c3160cf2d03dfd0954d7014ebe4d72972eb
scsk-hasegawa <tgr724271@gmail.com>
2023-07-06 15:13:31 +0900

    メッセージ

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import { init as initApm } from '@elastic/apm-rum'
-var apm = initApm({
-
-  // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
-  serviceName: 'my-service-name',
-
-  // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: 'https://1ecc7c85ee2a41c5b7be35e515b650d3.apm.ap-northeast-1.aws.cloud.es.io:443',
-
-  // Set the service version (required for source map feature)
-  serviceVersion: '',
-
-  // Set the service environment
-  environment: 'my-environment'
-})
 
 function App() {
   return (
--- src/App.test.js
+++ src/App.test.js
@@ -1,25 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-import { init as initApm } from '@elastic/apm-rum'
-var apm = initApm({
-
-  // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
-  serviceName: 'my-service-name',
-
-  // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: 'https://1ecc7c85ee2a41c5b7be35e515b650d3.apm.ap-northeast-1.aws.cloud.es.io:443',
-
-  // Set the service version (required for source map feature)
-  serviceVersion: '',
-
-  // Set the service environment
-  environment: 'my-environment'
-})
-
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
